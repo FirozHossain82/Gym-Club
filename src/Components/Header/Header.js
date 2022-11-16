b8d27e05536524b08d2c875fa6839eba66f50f7f
FirozHossain82 <firozhossain295@gmail.com>
2022-11-16 13:14:41 +0600

    tailwind and google fonts commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+            <h1>Welcome to my Gym House</h1>
+            <button className="btn btn-active btn-primary">Button</button>
         </div>
     );
 };
