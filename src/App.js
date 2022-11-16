aae9548796416dce27c893011d2826d9b51754a8
FirozHossain82 <firozhossain295@gmail.com>
2022-11-17 01:01:44 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import GymWorks from './Components/GymWorks/GymWorks';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App bg-cyan-100 ">
         <Header></Header>
         <GymWorks></GymWorks>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className=' bg-indigo-200 py-6 text-center font-serif  font-bold text-sm'>
+             <h4>@All Right Reserved by Md Firoz Hossain</h4>
+        </div>
+    );
+};
+
+export default Footer;
