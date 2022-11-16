c9a4a8f220019765cd6024914751ba2c15889d5c
FirozHossain82 <firozhossain295@gmail.com>
2022-11-16 13:26:14 +0600

    font awesome links added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to my Gym House</h1>
+            <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>
             <button className="btn btn-active btn-primary">Button</button>
         </div>
     );
